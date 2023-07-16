8fe44fe5ea08ed0c1454c24e8b2984dbb1ce97f6
shewalesaurabh <138956528+shewalesaurabh@users.noreply.github.com>
2023-07-16 15:28:25 +0530

    Add project files.

---
+++ ProductMgmt/Controllers/ProductsController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductMgmt.Models;
+
+namespace ProductMgmt.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: ProductsController
+        public ActionResult Index()
+        {
+            List<Product> list = Product.AllProduct();
+            return View(list);
+        }
+
+        // GET: ProductsController/Details/5
+        public ActionResult Details(int id)
+        {
+            Product obj = Product.GetProduct(id);
+            return View(obj);
+        }
+
+        // GET: ProductsController/Create
+        public ActionResult Create()
+        {
+            Product product = new Product();    
+            return View(product);
+        }
+
+        // POST: ProductsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                Product obj = new Product();
+                obj.ProductID = Convert.ToInt32(collection["ProductID"]);
+                obj.ProductName = Convert.ToString(collection["ProductName"]);
+                obj.Price = Convert.ToDecimal(collection["Price"]);
+
+
+                Product.AddProduct(obj);
+                ViewBag.Message = "Product Added Succesfully";
+                return View();
+            }
+            catch(Exception e)
+            {
+                ViewBag.Message = e.Message;
+                return View();
+            }
+        }
+
+        // GET: ProductsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Product product =Product.GetProduct(id);
+            return View(product);
+        }
+
+        // POST: ProductsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Product obj)
+        {
+            try
+            {
+
+                Product.EditProduct(obj);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Product product = Product.GetProduct(id);
+            return View(product);
+        }
+
+        // POST: ProductsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                Product.DeleteProduct(id);
+                ViewBag.Meassage = "Product Deleted";
+
+                // return View();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e)
+            {
+                ViewBag.Message = e.Message;
+                return View();
+            }
+        }
+    }
+}
---
+++ ProductMgmt/Models/Product.cs
@@ -0,0 +1,190 @@
+﻿using System.Data;
+using System.Data.SqlTypes;
+using System.Reflection;
+using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ProductMgmt.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public Decimal Price { get; set; }
+
+        public static List<Product> AllProduct()
+        {
+            List<Product> lstProd = new List<Product>();
+
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=KTjune23;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            cn.Open();
+
+            try
+            {
+
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from Products";
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Product product = new Product();
+                    product.ProductID = dr.GetInt32("ProductID");
+                    product.ProductName = dr.GetString("ProductName");
+                    product.Price = dr.GetDecimal("Price");
+                    lstProd.Add(product);              
+                }
+
+                dr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return lstProd;
+        }
+
+        
+        public static Product GetProduct(int ProductID)
+        {
+            Product product = new Product();
+
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=KTjune23;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            cn.Open();
+
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from Products where ProductID=@ProductID";
+                cmd.Parameters.AddWithValue("@ProductID", ProductID);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    product.ProductID = dr.GetInt32("ProductID");
+                    product.ProductName = dr.GetString("ProductName");
+                    product.Price = dr.GetDecimal("Price");
+                }
+                dr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return product;
+        }
+
+
+        public static void AddProduct(Product obj)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=KTjune23;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            cn.Open();
+
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "InsertProduct";
+
+                cmd.Parameters.AddWithValue("@ProductID", obj.ProductID);
+                cmd.Parameters.AddWithValue("@ProductName", obj.ProductName);
+                cmd.Parameters.AddWithValue("@Price", obj.Price);
+                
+
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+
+        public static void DeleteProduct(int ProductID)
+        {
+
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=KTjune23;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;MultipleActiveResultSets=true";
+            cn.Open();
+
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Delete Products where ProductID=@ProductID";
+                cmd.Parameters.AddWithValue("@ProductID", ProductID);
+
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+           
+        }
+
+        public static Product EditProduct(Product obj)
+        {
+            Product product = new Product();
+
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=KTjune23;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;MultipleActiveResultSets=true";
+            cn.Open();
+
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Update Products set ProductID=@ProductID , ProductName=@ProductName , Price=@Price where @ProductID=@ProductID";
+
+                //cmd.CommandType = CommandType.StoredProcedure;
+                //cmd.CommandText = "UpdateProduct";
+                cmd.Parameters.AddWithValue("@ProductID", obj.ProductID);
+                cmd.Parameters.AddWithValue("@ProductName", obj.ProductName);
+                cmd.Parameters.AddWithValue("@Price", obj.Price);
+
+
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return product;
+        }
+
+
+    }
+
+
+}
